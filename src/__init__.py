5aacc1e4b25f0a28a96d85e1ef403161ead3dc6f
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-29 06:49:38 -0300

    ajuste de modelos para la db, luego los tendré que ubicar mejor

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,17 @@ from flask_cors import CORS
 
 from src.persistence.repository import RepositoryManager
 
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+
+
+
+
+
+
+
+
+
 cors = CORS()
 repo = RepositoryManager()
 
@@ -15,6 +26,96 @@ def create_app(config_class="src.config.DevelopmentConfig") -> Flask:
     The default configuration class is DevelopmentConfig.
     """
     app = Flask(__name__)
+
+##############################################################################################
+#                                           SQLAlchemy                                       #
+##############################################################################################
+    #if app.debug:
+    print("DEBUUUUUUUUUUUUUUUUUUUUUUUUUG MOOOOOOOODEEEEEEE")
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
+    db = SQLAlchemy(app)
+
+
+
+
+
+    class User(db.Model):
+        __tablename__ = 'users'
+
+        id = db.Column(db.String(36), primary_key=True)
+        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+        updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        first_name = db.Column(db.String(120), nullable=False)
+        last_name = db.Column(db.String(120), nullable=False)
+        password = db.Column(db.String(128), nullable=False)
+        is_admin = db.Column(db.Boolean, default=False)
+
+    class Place(db.Model):
+        __tablename__ = 'places'
+
+        id = db.Column(db.String(36), primary_key=True)
+        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+        updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+        name = db.Column(db.String(120), nullable=False)
+        description = db.Column(db.String(256))
+        address = db.Column(db.String(256), nullable=False)
+        latitude = db.Column(db.Float, nullable=False)
+        longitude = db.Column(db.Float, nullable=False)
+        host_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+        city_id = db.Column(db.String(36), db.ForeignKey('cities.id'), nullable=False)
+        price_per_night = db.Column(db.Integer, nullable=False)
+        number_of_rooms = db.Column(db.Integer, nullable=False)
+        number_of_bathrooms = db.Column(db.Integer, nullable=False)
+        max_guests = db.Column(db.Integer, nullable=False)
+
+        __table_args__ = (
+        CheckConstraint('price_per_night >= 0', name='check_price_per_night_non_negative'),
+        CheckConstraint('max_guests >= 0', name='check_max_guests_non_negative'),
+        )
+
+    class Review(db.Model):
+        __tablename__ = 'reviews'
+
+        id = db.Column(db.String(36), primary_key=True)
+        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+        updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+        place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+        user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+        comment = db.Column(db.String(256))
+        rating = db.Column(db.Float, nullable=False)
+
+    class City(db.Model):
+        __tablename__ = 'cities'
+
+        id = db.Column(db.String(36), primary_key=True)
+        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+        updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+        name = db.Column(db.String(120), nullable=False)
+        country_code = db.Column(db.String(2), db.ForeignKey('countries.code'), nullable=False)
+
+    class Country(db.Model):
+        __tablename__ = 'countries'
+
+        name = db.Column(db.String(120), nullable=False)
+        code = db.Column(db.String(2), primary_key=True)
+
+    class Amenity(db.Model):
+        __tablename__ = 'amenities'
+
+        id = db.Column(db.String(36), primary_key=True)
+        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+        updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())
+        name = db.Column(db.String(120), nullable=False)
+
+
+##############################################################################################
+#                                           SQLAlchemy                                       #
+##############################################################################################
+
+
+
+
     app.url_map.strict_slashes = False
 
     app.config.from_object(config_class)
