b7d063ac9d9def820395a102232ee5714960d643
GaboAfk <gabrielmorffe95@gmail.com>
2024-06-29 06:50:21 -0300

    posible lugar para insertar los modelos db

--- src/persistence/db.py
+++ src/persistence/db.py
@@ -15,9 +15,27 @@
 from src.models.base import Base
 from src.persistence.repository import Repository
 
+#from src import db
 
 class DBRepository(Repository):
     """Dummy DB repository"""
+
+    """class User(db.Model):
+        id = db.Column(db.String(36), primary_key=True)
+        email = db.Column(db.String(120), unique=True, nullable=False)
+        password = db.Column(db.String(128), nullable=False)
+        is_admin = db.Column(db.Boolean, default=False)
+        created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+        updated_at = db.Column(db.DateTime, onupdate=db.func.current_timestamp())"""
+
+
+
+
+
+
+
+
+
 
     def __init__(self) -> None:
         """Not implemented"""
